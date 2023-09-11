f7db56aa1b1d78251e549c3b1f0563bb60aa53d5
David Yappeter <davidyap11les@gmail.com>
2023-09-11 17:24:54 +0700

    feat: initialize project (with theme)

---
+++ app/src/main/java/myplayground/example/jakpost/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package myplayground.example.jakpost.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Cloudflare color palette
+val PrincetonOrange = Color(0xFFF48120)
+val YellowOrange = Color(0xFFFAAD3F)
+val DeepSaffron = Color(0xFFF7972F)
+val Arsenic = Color(0xFF404041)
+
+// Base Color
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val LightStroke = Color(0xFFCCCCCC)
+val Disabled = Color(0xFFEBEBE4)
+val Subtitle = Color(0xFF6E6E6E)
---
+++ app/src/main/java/myplayground/example/jakpost/ui/theme/Theme.kt
@@ -0,0 +1,160 @@
+package myplayground.example.jakpost.ui.theme
+
+import android.app.Activity
+import android.content.Context
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+@Composable
+private fun customDynamicLightColorScheme(context: Context): ColorScheme {
+    val dynamicC = dynamicLightColorScheme(context)
+    return remember {
+        ColorScheme(
+            primary = PrincetonOrange,
+            secondary = YellowOrange,
+            tertiary = DeepSaffron,
+            background = White,
+            primaryContainer = PrincetonOrange,
+
+            onPrimary = White,
+            onSecondary = White,
+            onTertiary = White,
+            onSurface = Black,
+            onPrimaryContainer = White,
+
+            error = dynamicC.error,
+            errorContainer = dynamicC.errorContainer,
+            inverseOnSurface = dynamicC.inverseOnSurface,
+            inversePrimary = dynamicC.inversePrimary,
+            inverseSurface = dynamicC.inverseSurface,
+            onBackground = dynamicC.onBackground,
+            onError = dynamicC.onError,
+            onErrorContainer = dynamicC.onErrorContainer,
+            onSecondaryContainer = dynamicC.onSecondaryContainer,
+            onSurfaceVariant = dynamicC.onSurfaceVariant,
+            onTertiaryContainer = dynamicC.onTertiaryContainer,
+            outline = dynamicC.outline,
+            outlineVariant = dynamicC.outlineVariant,
+            scrim = dynamicC.scrim,
+            secondaryContainer = dynamicC.onSecondaryContainer,
+            surface = dynamicC.surface,
+            surfaceTint = dynamicC.surfaceTint,
+            surfaceVariant = dynamicC.onSurfaceVariant,
+            tertiaryContainer = dynamicC.tertiaryContainer,
+        )
+    }
+}
+
+@Composable
+private fun customDynamicDarkColorScheme(context: Context): ColorScheme {
+    val dynamicC = dynamicDarkColorScheme(context)
+    return remember {
+        ColorScheme(
+            primary = PrincetonOrange,
+            secondary = YellowOrange,
+            tertiary = DeepSaffron,
+            background = Arsenic,
+            primaryContainer = PrincetonOrange,
+
+            onPrimary = White,
+            onSecondary = White,
+            onTertiary = White,
+            onSurface = White,
+            onPrimaryContainer = White,
+
+            error = dynamicC.error,
+            errorContainer = dynamicC.errorContainer,
+            inverseOnSurface = dynamicC.inverseOnSurface,
+            inversePrimary = dynamicC.inversePrimary,
+            inverseSurface = dynamicC.inverseSurface,
+            onBackground = dynamicC.onBackground,
+            onError = dynamicC.onError,
+            onErrorContainer = dynamicC.onErrorContainer,
+            onSecondaryContainer = dynamicC.onSecondaryContainer,
+            onSurfaceVariant = dynamicC.onSurfaceVariant,
+            onTertiaryContainer = dynamicC.onTertiaryContainer,
+            outline = dynamicC.outline,
+            outlineVariant = dynamicC.outlineVariant,
+            scrim = dynamicC.scrim,
+            secondaryContainer = dynamicC.onSecondaryContainer,
+            surface = dynamicC.surface,
+            surfaceTint = dynamicC.surfaceTint,
+            surfaceVariant = dynamicC.onSurfaceVariant,
+            tertiaryContainer = dynamicC.tertiaryContainer,
+        )
+    }
+}
+
+private val DarkColorScheme = darkColorScheme(
+    primary = PrincetonOrange,
+    secondary = YellowOrange,
+    tertiary = DeepSaffron,
+    background = Arsenic,
+    primaryContainer = PrincetonOrange,
+
+    onPrimary = White,
+    onSecondary = White,
+    onTertiary = White,
+    onSurface = White,
+    onPrimaryContainer = White,
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = PrincetonOrange,
+    secondary = YellowOrange,
+    tertiary = DeepSaffron,
+    background = White,
+    primaryContainer = PrincetonOrange,
+
+    onPrimary = White,
+    onSecondary = White,
+    onTertiary = White,
+    onSurface = Black,
+    onPrimaryContainer = White,
+)
+
+@Composable
+fun JakPostTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(), // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true, content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme)
+                customDynamicDarkColorScheme(context)
+            else
+                customDynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    val context = LocalContext.current
+
+    MaterialTheme(
+        colorScheme = colorScheme, typography = Typography, content = content
+    )
+}
